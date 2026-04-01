dcea3795c174c9abe724bf5cc0d8a291d4cde84a
smijis <shinselina@hotmail.com>
2026-04-01 12:09:50 +1100

    added postcodes

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models 
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class CustomUser(AbstractUser): #inheriting the built in default django AbstractUser class. Common practice to create your own "CustomUser" class
     is_active = models.BooleanField(default=True)
     name = models.CharField(max_length=200)
     suburb = models.CharField(max_length=200)
+    postcode = models.IntegerField(
+    null=True, 
+    blank=True,
+    validators=[MinValueValidator(1000), MaxValueValidator(9999)])
     def __str__(self): 
         return self.username #when you print to the console, you will get the username of the user
 #make sure to edit the settings.py in crowdfunding to tell Django to use the CustomUser authentication
